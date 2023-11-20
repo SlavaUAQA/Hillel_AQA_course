40359ef264df87fbc1961db5707aa2c9bd308a1a
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-11-20 14:15:30 +0200

    Update hometask_5.py
    optimisation

--- hometask_5.py
+++ hometask_5.py
@@ -1,19 +1,28 @@
-dict_actions = {"+":range(2, 10), "-":range(2, 10), "*":range(2, 10), "/":range(2, 10)}
+# dict_actions = {"+":range(2, 10), "-":range(2, 10), "*":range(2, 10), "/":range(2, 10)}
+# user_request = input("Введіть математичну дію, яку ви хочете надрукувати ('+', '-', '*' чи '/'):")
+# if user_request == "+":
+#     for i in dict_actions["+"]:
+#         for j in dict_actions["+"]:
+#             print(f"{i}+{j}={i+j}", end=" \n",)
+# elif user_request == "-":
+#     for i in dict_actions["-"]:
+#         for j in dict_actions["-"]:
+#             print(f"{i}-{j}={i-j}", end=" \n")
+# elif user_request == "*":
+#     for i in dict_actions["*"]:
+#         for j in dict_actions["*"]:
+#             print(f"{i}*{j}={i*j}", end=" \n")
+# elif user_request == "/":
+#     for i in dict_actions["/"]:
+#         for j in dict_actions["/"]:
+#             print(f"{i}/{j}={i/j}", end=" \n")
+# print()
+
+dict_actions = {"+": range(2, 10), "-": range(2, 10), "*": range(2, 10), "/": range(2, 10)}
 user_request = input("Введіть математичну дію, яку ви хочете надрукувати ('+', '-', '*' чи '/'):")
-if user_request == "+":
-    for i in dict_actions["+"]:
-        for j in dict_actions["+"]:
-            print(f"{i}+{j}={i+j}", end=" \n",)
-elif user_request == "-":
-    for i in dict_actions["-"]:
-        for j in dict_actions["-"]:
-            print(f"{i}-{j}={i-j}", end=" \n")
-elif user_request == "*":
-    for i in dict_actions["*"]:
-        for j in dict_actions["*"]:
-            print(f"{i}*{j}={i*j}", end=" \n")
-elif user_request == "/":
-    for i in dict_actions["/"]:
-        for j in dict_actions["/"]:
-            print(f"{i}/{j}={i/j}", end=" \n")
+if user_request in dict_actions:
+    for i in dict_actions[user_request]:
+        for j in dict_actions[user_request]:
+            result = (f"{i}{user_request}{j}")
+            print(f"{i}{user_request}{j} = {eval(result)}")
 print()
