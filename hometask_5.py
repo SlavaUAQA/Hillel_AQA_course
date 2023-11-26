34b85442d1fa235c938f7bef43eadc79838fdbb1
Serhii Sheheda <sergiy.shegeda@gmail.com>
2023-11-26 18:58:19 +0100

    bug/1919 adding TODO

--- hometask_5.py
+++ hometask_5.py
@@ -1,3 +1,4 @@
+#TODO: 1. Написати програму, яка буде зчитувати з консолі число.
 # dict_actions = {"+":range(2, 10), "-":range(2, 10), "*":range(2, 10), "/":range(2, 10)}
 # user_request = input("Введіть математичну дію, яку ви хочете надрукувати ('+', '-', '*' чи '/'):")
 # if user_request == "+":
@@ -23,6 +24,6 @@ user_request = input("Введіть математичну дію, яку ви 
 if user_request in dict_actions:
     for i in dict_actions[user_request]:
         for j in dict_actions[user_request]:
-            result = (f"{i}{user_request}{j}")
+            result = f"{i}{user_request}{j}"
             print(f"{i}{user_request}{j} = {eval(result)}")
 print()
