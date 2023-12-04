908adcad57f13d5766958825df5837e55e51d298
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-04 18:55:13 +0200

    Create hometask_7_1.py
    functions

---
+++ hometask_7_1.py
@@ -0,0 +1,13 @@
+def numbers(list_numbers = [100, 4, 11, 777, 50, 10, 999]):
+    sorted_list = sorted(list_numbers)
+    return sorted_list
+
+
+def numbers_2(list_numbers = [1, 14, 175, 1000, 5, 0, 2]):
+    sorted_list = sorted(list_numbers, reverse=True)
+    return sorted_list
+
+
+def words_list(list_words = ["оставь", "надежду", "всяк", "сюда", "входящий"]):
+    sorted_words_list = sorted(list_words)
+    return sorted_words_list
