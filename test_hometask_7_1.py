e258dfd581991e9b91f987cb9882a38dd0e5b548
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-04 19:01:01 +0200

    Create test_hometask_7_1.py
    test functions

---
+++ test_hometask_7_1.py
@@ -0,0 +1,18 @@
+import pytest
+from hometask_7_1 import numbers
+from hometask_7_1 import numbers_2
+from hometask_7_1 import words_list
+def test_numbers():
+    result = numbers()
+    for i in range(len(result) - 1):
+        assert result[i] <= result[i + 1], "Список не отсортирован"
+
+def test_numbers_2():
+    result = numbers_2()
+    for i in range(len(result) - 1):
+        assert result[i] >= result[i+1], "Список не отсортирован"
+
+def test_words_list():
+    result = words_list()
+    for i in range(len(result) - 1):
+        assert result[i] <= result[i + 1], "Список не отсортирован"
