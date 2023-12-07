1f4951c3eea19aac4b98ffe10dcb16a48a506694
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-07 20:09:52 +0200

    Update test_hometask_7_1.py
    some changes range -->> enumerate

--- test_hometask_7_1.py
+++ test_hometask_7_1.py
@@ -2,17 +2,18 @@ import pytest
 from hometask_7_1 import numbers
 from hometask_7_1 import numbers_2
 from hometask_7_1 import words_list
+
 def test_numbers():
     result = numbers()
-    for i in range(len(result) - 1):
-        assert result[i] <= result[i + 1], "Список не отсортирован"
+    for i, number in enumerate(result[:-1]):
+        assert number <= result[i + 1], "Список не отсортирован"
 
 def test_numbers_2():
     result = numbers_2()
-    for i in range(len(result) - 1):
-        assert result[i] >= result[i+1], "Список не отсортирован"
+    for i, number in enumerate(result[:-1]):
+        assert number >= result[i + 1], "Список не отсортирован"
 
 def test_words_list():
     result = words_list()
-    for i in range(len(result) - 1):
+    for i, word in enumerate(result[:-1]):
         assert len(result[i]) <= len(result[i + 1]), "Список не отсортирован"
