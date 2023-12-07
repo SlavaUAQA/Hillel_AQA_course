599d044e4683d603c461e2d9789a0b2d87115fc8
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-07 20:08:13 +0200

    Update hometask_7_1.py
    some changes mostly related to data type

--- hometask_7_1.py
+++ hometask_7_1.py
@@ -1,13 +1,13 @@
-def numbers(list_numbers = [100, 4, 11, 777, 50, 10, 999]):
-    sorted_list = sorted(list_numbers)
-    return sorted_list
+def numbers(tuple_numbers=(100, 4, 11, 777, 50, 10, 999)):
+    sorted_tuple = sorted(tuple_numbers)
+    return sorted_tuple
 
 
-def numbers_2(list_numbers = [1, 14, 175, 1000, 5, 0, 2]):
-    sorted_list = sorted(list_numbers, reverse=True)
-    return sorted_list
+def numbers_2(tuple_numbers=[1, 14, 175, 1000, 5, 0, 2]):
+    sorted_tuple = sorted(tuple_numbers, reverse=True)
+    return sorted_tuple
 
 
-def words_list(list_words = ["оставь", "надежду", "всяк", "сюда", "входящий"]):
-    sorted_words_list = sorted(list_words, key = len)
-    return sorted_words_list
+def words_list(tuple_words=["оставь", "надежду", "всяк", "сюда", "входящий"]):
+    sorted_words_tuple = sorted(tuple_words, key=len)
+    return sorted_words_tuple
