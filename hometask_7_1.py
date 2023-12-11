a4514b6a0ed2e176fab372faf7a81086fd2f3f7d
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-11 18:14:18 +0200

    Update hometask_7_1.py
    value moved inside the function
    some trivial changes

--- hometask_7_1.py
+++ hometask_7_1.py
@@ -1,13 +1,18 @@
-def numbers(tuple_numbers=(100, 4, 11, 777, 50, 10, 999)):
-    sorted_tuple = sorted(tuple_numbers)
+def numbers(tuple_numbers=()):
+    tuple_inside = tuple_numbers
+    tuple_inside = (100, 4, 11, 777, 50, 10, 999)
+    sorted_tuple = sorted(tuple_inside)
     return sorted_tuple
 
-
-def numbers_2(tuple_numbers=[1, 14, 175, 1000, 5, 0, 2]):
-    sorted_tuple = sorted(tuple_numbers, reverse=True)
+def numbers_2(tuple_numbers=()):
+    tuple_inside = tuple_numbers
+    tuple_inside = (100, 4, 11, 777, 50, 10, 999)
+    sorted_tuple = sorted(tuple_inside, reverse=True)
     return sorted_tuple
 
-
-def words_list(tuple_words=["оставь", "надежду", "всяк", "сюда", "входящий"]):
-    sorted_words_tuple = sorted(tuple_words, key=len)
+def words_list(tuple_words=()):
+    words_inside_tuple = words_list
+    words_inside_tuple = ["оставь", "надежду", "всяк", "сюда", "входящий"]
+    sorted_words_tuple = sorted(words_inside_tuple, key=len)
     return sorted_words_tuple
+
