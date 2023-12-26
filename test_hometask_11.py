74a998eaaf85092223e30691652050b46d9ae894
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-26 15:47:42 +0200

    Create test_hometask_11.py
    ht - 11 tests first try

---
+++ test_hometask_11.py
@@ -0,0 +1,27 @@
+# task - 2
+import time
+
+
+def test_sleep_1():
+    time.sleep(2)
+    True
+
+
+def test_sleep_2():
+    time.sleep(2)
+    True
+
+
+def test_sleep_3():
+    time.sleep(2)
+    True
+
+
+def test_sleep_4():
+    time.sleep(2)
+    True
+
+
+def test_individual():
+    time.sleep(2)
+    True
