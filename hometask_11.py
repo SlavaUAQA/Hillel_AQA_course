8e2164eb27407510e2d8025bcef751cbf35a438f
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2023-12-28 10:37:15 +0200

    Update hometask_11.py
    object.name is added
    extra class is removed matching to hometask rules

--- hometask_11.py
+++ hometask_11.py
@@ -13,22 +13,9 @@ class Student1:
         return intro_grade
 
 
-class Student2:
-    name = "Viacheslav"
-    grades = ["5a", "4c", "5b", "3d", "5a"]
-
-    def greeetings(self):
-        hello = f'Привіт, мене звати {self.name}.'
-        return hello
-
-    def grade_list(self):
-        self.grades = ", ".join(self.grades)
-        intro_grade = f'Мої оцінки: {self.grades}'
-        return intro_grade
-
-
 obj_student1 = Student1()
-obj_student2 = Student2()
+obj_student2 = Student1()
+obj_student2.name = "Viacheslav"
 print(f'{obj_student1.greeetings()}\n{obj_student1.grade_list()}')
 print()
 print(f'{obj_student2.greeetings()}\n{obj_student2.grade_list()}')
