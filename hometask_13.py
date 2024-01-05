4c80e0fefa7a95bb8f58b92a6b33ac2e90b0b170
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-01-05 16:26:23 +0200

    Update hometask_13.py

--- hometask_13.py
+++ hometask_13.py
@@ -7,10 +7,14 @@ class IsGreater:
         if isinstance(number, int):
             return [i > number for i in self.list]
         else:
-            return TypeError
+            return NotImplemented
 
 
 schedule_values = [2, 5, 8, 10, 15, 20]
+schedule_values_2 = [1, 3, 100, 5, 17, 50]
 is_greater_number = IsGreater(schedule_values)
+is_greater_number_2 = IsGreater(schedule_values_2)
+
 
 print(is_greater_number.__gt__(10))
+print(is_greater_number_2.__gt__(10))
