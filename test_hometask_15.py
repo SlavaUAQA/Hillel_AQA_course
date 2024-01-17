2fd85f3f77ced85d94963e1b324744bb288ce60f
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-01-17 13:05:49 +0200

    Update test_hometask_15.py
    separation fixture to two different

--- test_hometask_15.py
+++ test_hometask_15.py
@@ -7,15 +7,19 @@ from datetime import datetime
 class TestCalculator:
 
     @pytest.fixture
-    def start_and_end(self):
+    def setup(self):
         current_time = datetime.now()
         with open("log.txt", "a") as file:
             file.write(f"Ми стартовали тест в {current_time}\n")
         yield
+
+    @pytest.fixture
+    def teardown(self):
         with open("log.txt", "a") as file:
+            current_time = datetime.now()
             file.write(f"Ми закінчили тест в {current_time}\n")
 
-    @pytest.mark.usefixtures("start_and_end")
+    @pytest.mark.usefixtures("setup", "teardown")
     @pytest.mark.parametrize("value_1, value_2, expected_result", [
         (5, 10, 15),
         (5, 0, 5),
@@ -28,7 +32,7 @@ class TestCalculator:
         result = calc.adds(value_1, value_2)
         assert result == expected_result, f"Expected {expected_result}, but got {result}"
 
-    @pytest.mark.usefixtures("start_and_end")
+    @pytest.mark.usefixtures("setup", "teardown")
     @pytest.mark.parametrize("value_1, value_2, expected_result", [
         (100, 2, 50),
         (100, -5, -20),
