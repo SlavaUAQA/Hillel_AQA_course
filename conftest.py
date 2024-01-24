fb871f4046b2129666e53e1de99806f1ee307e11
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-01-24 15:26:13 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,18 +4,10 @@ from selenium import webdriver
 import requests
 
 
-@pytest.fixture(scope="class")
-def fixture_Chuck(request):
-    response = requests.request(method="GET", url="https://api.chucknorris.io/jokes/search?query=first")
-    status_code = response.status_code
-    request.cls.response = response
-    request.cls.status_code = status_code
-    yield response, status_code
-
 @pytest.fixture
 def chrome():
     options = webdriver.ChromeOptions()
-    options.add_argument("executable_path=C:\Program Files\chromedriver-win64\chromedriver.exe")
+    options.add_argument("executable_path=/Users/viacheslavzakurenko/Downloads/chromedriver-mac-arm64/chromedriver")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
