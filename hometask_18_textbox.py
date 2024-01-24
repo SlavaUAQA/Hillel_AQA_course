57df455643e3c0d5e65aad4d469336e48b031675
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-01-24 15:24:11 +0100

    Update hometask_18_textbox.py

--- hometask_18_textbox.py
+++ hometask_18_textbox.py
@@ -3,18 +3,20 @@ from selenium.webdriver.common.by import By
 
 
 class TextBoxPage:
-    def __int__(self, driver: WebDriver):
+    def __init__(self, driver: WebDriver):
         self.url = "https://demoqa.com/text-box"
         self.driver = driver
         self.full_name_field = (By.ID, "userName")
-        self.email_field = (By.ID, "userEmail")
-        self.current_address_filed = (By.ID, "currentAddress")
-        self.permanent_address_field = (By.ID, "permanentAddress")
+        self.email_field = (By.CSS_SELECTOR, "#userEmail")
+        self.current_address_filed = (
+        By.XPATH, "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[3]/div[2]/textarea")
+        self.permanent_address_field = (
+        By.XPATH, "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[4]/div[2]/textarea")
         self.submit_button = (By.ID, "submit")
         self.result_fullname = (By.CSS_SELECTOR, "#name")
         self.result_email = (By.CSS_SELECTOR, "#email")
-        self.result_curr_addr = (By.CSS_SELECTOR, "#currentAddress")
-        self.result_perm_addr = (By.CSS_SELECTOR, "#permanentAddress")
+        self.result_curr_addr = (By.XPATH, "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[6]/div/p")
+        self.result_perm_addr = (By.XPATH, "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[6]/div/p")
 
     def open(self):
         self.driver.get(self.url)
@@ -57,13 +59,16 @@ class TextBoxPage:
         return None
 
     def get_fullname(self) -> str:
-        self.driver.find_element(*self.result_fullname).text
+        return self.driver.find_element(*self.result_fullname).text
 
     def get_email(self) -> str:
-        self.driver.find_element(*self.result_email).text
+        return self.driver.find_element(*self.result_email).text
 
     def get_curr_addr(self) -> str:
-        self.driver.find_element(*self.result_curr_addr).text
+        return self.driver.find_element(*self.result_curr_addr).text
 
     def get_perm_addr(self) -> str:
-        self.driver.find_element(*self.result_perm_addr).text
+        return self.driver.find_element(*self.result_perm_addr).text
+
+    def scroll_down(self) -> None:
+        self.driver.execute_script("window.scrollBy(0, 500);")
