e8bc55e1b4f71d2b9b4ec19c9cbd403b51d14cf8
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-02-02 17:00:05 +0100

    Create hometask19_elementspage.py

---
+++ hometask19_elementspage.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+class ElementsPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://demoqa.com/elements"
+        self.element_categories = By.XPATH, "//div[contains(@class, show)]//li"
+
+    def open(self):
+        self.driver.get(self.url)
+        return self
+
+    def get_elements_categories(self):
+        categories = [cat.text for cat in self.driver.find_elements(*self.element_categories)]
+        return categories
+
+def obj1():
+    a = ElementsPage(webdriver.Chrome())
+    a.open()
+    for i in a.get_elements_categories():
+        print(i)
+    print(len(a.get_elements_categories()))
+
+obj1()
