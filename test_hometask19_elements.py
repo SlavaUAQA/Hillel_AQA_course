732897d9d2f66ae8352d8cdd1a68cdd856a57a97
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-02-06 15:19:31 +0100

    Update test_hometask19_elements.py
    extra values are removed
    webrdiver switched from chrome to firefox

--- test_hometask19_elements.py
+++ test_hometask19_elements.py
@@ -1,24 +1,21 @@
     import pytest
-from hometask19_elementspage import ElementsPage
-
-class TestElementsPage:
-    @pytest.mark.parametrize("expected", [
-        'Text Box',
-        'Check Box',
-        'Radio Button',
-        'Web Tables',
-        'Buttons',
-        'Links',
-        '',
-        '',
-        '',
-        '',
-        '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
-        ''
-    ])
-    def test_elements_response(self, chrome, expected):
-        page = ElementsPage(chrome)
-        page.open()
-        elements = page.get_elements_categories()
-        for element in elements:
-            assert element in expected
+    from hometask19_elementspage import ElementsPage
+    
+    class TestElementsPage:
+        @pytest.mark.parametrize("expected", [
+            'Text Box',
+            'Check Box',
+            'Radio Button',
+            'Web Tables',
+            'Buttons',
+            'Links',
+            None,
+        ])
+        def test_elements_response(self, firefox, expected):
+            page = ElementsPage(firefox)
+            page.open()
+            elements = page.get_elements_categories()
+            if expected is not None:
+                assert expected in elements
+            else:
+                assert expected not in elements
