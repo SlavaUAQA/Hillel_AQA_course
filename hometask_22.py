19f667da8200ca0d173794132adf5021204d0bca
Viacheslav Zakurenko <149477074+SlavaUAQA@users.noreply.github.com>
2024-03-04 14:29:55 +0200

    Update hometask_22.py

--- hometask_22.py
+++ hometask_22.py
@@ -1,7 +1,5 @@
 import datetime
-
 import requests
-
 
 class CurrencyExchange:
     def get_api(self):
@@ -9,13 +7,17 @@ class CurrencyExchange:
         response = requests.get(url)
         return response.json()
 
+    def convert_currency_rate(self, response):
+        currency_convert = ""
+        for number, currency in enumerate(response, start=1):
+            currency_convert += f"{number}. {currency['txt']} to UAH: {currency['rate']}\n"
+        return currency_convert
+
     def save_currency_rate(self, response, filename):
         with open(filename, 'w', encoding='utf-8') as file:
             file.write(f"{datetime.date.today()}\n\n")
-            for number, currency in enumerate(response, start=1):
-                file.write(f"{number}. {currency['txt']} to UAH: {currency['rate']}\n")
-
-
+            file.write(self.convert_currency_rate(response))
+            
 currency_exchange = CurrencyExchange()
 data = currency_exchange.get_api()
 currency_exchange.save_currency_rate(data, "currency_rate.txt")
